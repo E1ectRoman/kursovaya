9ed530fc03bc69777d8a2a89ebed6f4ac76ce920
ElectRoman <E1ectRoman>
2022-11-07 21:57:47 +0800

    11урок посмотрел 5 видео

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,12 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import {validator} from "../../utils/validator";
 import TextField from "../common/form/textField";
+import api from "../../api";
+import SelectField from "../common/form/selectField";
+import RadioField from "../common/form/radioField";
+import MultiSelectField from "../common/form/multiSelectField";
 
 const RegisterForm = () => {
-  const [data, setData] = useState({email: '', password: ''});
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    profession: '',
+    sex: 'мужской',
+    qualities: [],
+  });
   const [errors, setErrors] = useState({});
+  const [professions, setProfession] = useState();
+  const [qualities, setQualities] = useState({});
 
-  const handleChange = ({target}) => {
+
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => setProfession(data));
+    api.qualities.fetchAll().then((data) => setQualities(data));
+  }, []);
+
+  const handleChange = (target) => {
     setData(prevState => ({
       ...prevState,
       [target.name]: target.value
@@ -35,6 +53,10 @@ const RegisterForm = () => {
         {message: 'Пароль должен состоять минимум из 8 символов', value: 8},
 
     },
+    profession: {
+      isRequired:
+        {message: 'Обязательно выберите вашу профессию'},
+    }
   }
   const validate = () => {
     const errors = validator(data, validatorConfig)
@@ -65,7 +87,36 @@ const RegisterForm = () => {
                  onChange={handleChange}
                  error={errors.password}
       />
-      <button className='btn btn-primary w-100 mx-auto' disabled={!isValid}>Отправить</button>
+      <SelectField
+        defaultOption='Выбрать...'
+        options={professions}
+        onChange={handleChange}
+        value={data.profession}
+        error={errors.profession}
+        label='Выберите вашу профессию'
+      />
+      <RadioField
+        options={[
+          {name: 'Мужской', value: 'мужской'},
+          {name: 'Женский', value: 'женский'},
+          {name: 'Другое', value: 'другое'},
+        ]}
+        value={data.sex}
+        name='sex'
+        onChange={handleChange}
+        label='Выберите ваш пол'
+      />
+      <MultiSelectField
+        options={qualities}
+        onChange={handleChange}
+        name='qualities'
+        label='Выберите ваши качества'
+      />
+      <button className='btn btn-primary w-100 mx-auto'
+              disabled={!isValid}
+      >
+        Отправить
+      </button>
     </form>
   );
 };
